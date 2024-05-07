2289ca7c19ab6826a0af3019a7ee03d9c58f8b0f
ellidall <124914524+ellidall@users.noreply.github.com>
2024-05-07 03:17:32 +0300

    added affiliateListPage, base.html.twig + styles

---
+++ affiliates/src/Controller/AffiliateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AffiliateController extends AbstractController
+{
+    public function getAffiliateListPage(): Response
+    {
+        return $this->render('affiliate/affiliateListPage.html.twig', []);
+    }
+}
